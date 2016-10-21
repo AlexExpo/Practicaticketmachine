5b93133de59d9512152b42aa93f0accf74327e7d
alejandro <alejandro>
2016-10-21 13:53:08 +0200

    Arreglos en el cógido

--- TrainStation.java
+++ TrainStation.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class TrainStation
 {
     private MaquinaDeBillete maquina1;
@@ -29,27 +29,3 @@ public class TrainStation
     }
 
     }
-=======
- public class TrainStation
-{
-	private MaquinaDeTicket maquina1;
-	private MaquinaDeTicket maquina2;
-	private int dineroTotal;
-
-	public TrainStation()
-	{
-		maquina1 = new MaquinaDeTicket(300);
-		maquina2 = new MaquinaDeTicket(200);
-		dineroTotal = 0;
-  }
-  
-  public void imprimirTotalDelDinero()
-	{
-		dineroTotal = maquina1.obtenerTotal();
-		dineroTotal = dineroTotal + maquina2.obtenerTotal();
-    System.out.println(dineroTotal);
-	}
-	
-  
-}
->>>>>>> a2d8fe966cafdd5dc944bdd8786277295d4f7bd8
